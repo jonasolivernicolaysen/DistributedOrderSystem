4e2afe07cfef614602717b797cb28295e92b15d7
Jonas.Nicolaysen <jonasolivernicolaysen@gmail.com>
2026-02-26 11:48:21 +0100

    added neccessary database migrations

---
+++ src/PaymentService/Migrations/20260226104744_AddProcessedOrdersTable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessedOrdersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PaidAt",
+                table: "Payments",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId",
+                table: "Payments",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Payments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ProcessedOrders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MessageId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ProcessedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessedOrders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcessedOrders");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Payments");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PaidAt",
+                table: "Payments",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
